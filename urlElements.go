18a0c0353d012797d9eabe0e660f6b47122d1fe5
torabit <pujigami@gmail.com>
2023-05-21 14:21:07 +0900

    Update assigns value of a function to variable

--- urlElements.go
+++ urlElements.go
@@ -22,18 +22,18 @@ func NewURLElements(urlString string) *URLElements {
 }
 
 func (u *URLElements) extractID() string {
-	// 現在ではitunesもspotifyもpathの最後の要素がIDになっている
 	// URLの構成に厳密に依存しているため、URLの形式の変更には注意
-
 	var id string
 
+	// 現在ではitunesもspotifyもpathの最後の要素がIDになっている
 	parts := strings.Split(u.Path, "/")
 	idIndex := len(parts) - 1
 	id = parts[idIndex]
 
-	if len(u.Query()) != 0 { // もしクエリパラメーターが存在するなら、そちらのIDを優先して返す
+	queries := u.Query()
+	if len(queries) != 0 { // もしクエリパラメーターが存在するなら、そちらのIDを優先して返す
 		var queryValues []string
-		for _, value := range u.Query() {
+		for _, value := range queries {
 			queryValues = append(queryValues, value...)
 		}
 		id = queryValues[0]
